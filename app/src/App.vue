4dcff0e517401450ca2cf5778505a122a117713f
Alex_zhao <zgh0711@163.com>
2017-09-21 11:38:37 +0800

    加入底部导航

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,12 +2,20 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
+    <!--3，将组件写入页面-->
+    <BottomNav></BottomNav>
   </div>
 </template>
 
 <script>
+  //1,导入组件
+  import BottomNav from './components/BottomNav.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  //2,注册组件
+  components:{
+    BottomNav
+  }
 }
 </script>
 
---
+++ app/src/components/BottomNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="bottomNav">
+    <div class="tab">首页</div>
+    <div class="tab">我的</div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #bottomNav {
+    display: flex;
+    width: 100%;
+    height: 3rem;
+    line-height: 3rem;
+    padding: 0.5rem 0;
+    text-align: center;
+    border-top: 1px solid #999999;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+  }
+
+  .tab {
+    flex: 1;
+    text-align: center;
+    border-right: 1px solid #999999;
+
+  }
+</style>
